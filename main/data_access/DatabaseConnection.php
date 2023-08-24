8157211f21e7914ca018f986bbb95f6d1893ec62
LeLe <17qj@queensu.ca>
2023-08-24 16:04:52 -0400

    Create DatabaseConnection and it's Interface
    create php file for building connection to sql database

---
+++ main/data_access/DatabaseConnection.php
@@ -0,0 +1,30 @@
+<?php
+
+class DatabaseConnection implements DatabaseConnectionInterface {
+    private $host = 'localhost';
+    private $dbname = 'employee_management';
+    private $username = 'your_username';
+    private $password = 'your_password';
+    private $connection;
+
+    public function getConnection() {
+        $this->connection = null;
+
+        try {
+            // Create a new PDO connection
+            $this->connection = new PDO(
+                "mysql:host={$this->host};dbname={$this->dbname}",
+                $this->username,
+                $this->password
+            );
+            // Set error handling to exceptions
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            // Display connection error message
+            echo 'Connection Error: ' . $e->getMessage();
+        }
+
+        return $this->connection;
+    }
+}
+?>
